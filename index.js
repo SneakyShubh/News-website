68a2d303afcfcd5f918b28e454254699ea6bb857
Shubham <shubham.is.sharma2403@gmail.com>
2023-01-08 22:17:37 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function myFunction1() {
+    document.getElementById("csd").style.display = "none";
+    document.getElementById("jsd").style.display = "none";
+    document.getElementById("news1").style.display = "block";
+    document.getElementById("backbtn").innerHTML = "Go Back";
+}
+function myFunction2() {
+    document.getElementById("csd").style.display = "none";
+    document.getElementById("jsd").style.display = "none";
+    document.getElementById("news2").style.display = "block";
+    document.getElementById("backbtn").innerHTML = "Go Back";
+}
+function myFunction3() {
+    document.getElementById("csd").style.display = "none";
+    document.getElementById("jsd").style.display = "none";
+    document.getElementById("news3").style.display = "block";
+    document.getElementById("backbtn").innerHTML = "Go Back";
+}
+function myFunction4() {
+    document.getElementById("csd").style.display = "none";
+    document.getElementById("jsd").style.display = "none";
+    document.getElementById("news4").style.display = "block";
+    document.getElementById("backbtn").innerHTML = "Go Back";
+}
+function myFunction5() {
+    document.getElementById("csd").style.display = "none";
+    document.getElementById("jsd").style.display = "none";
+    document.getElementById("news5").style.display = "block";
+    document.getElementById("backbtn").innerHTML = "Go Back";
+}
+function goback(){
+    document.getElementById("csd").style.display = "block";
+    document.getElementById("jsd").style.display = "block";
+    document.getElementById("news1").style.display = "none";
+    document.getElementById("news2").style.display = "none";
+    document.getElementById("news3").style.display = "none";
+    document.getElementById("news4").style.display = "none";
+    document.getElementById("news5").style.display = "none";
+    document.getElementById("backbtn").innerHTML = "Home";
+}
